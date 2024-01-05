0815465aef09388fc4e787834d891eb75321cf74
mwoz123 <4487025+mwoz123@users.noreply.github.com>
2024-01-05 21:41:44 +0100

    build.sh with start tiem

--- build.sh
+++ build.sh
@@ -1,2 +1,5 @@
-flatpak-builder --ccache --disable-rofiles-fuse build com.opera.Opera.ffmpeg.yaml
+date
+rm build -R
+flatpak-builder --ccache --disable-rofiles-fuse build com.opera.Opera.ffmpeg.yaml || true
 #flatpak run org.flatpak.Builder --ccache --disable-rofiles-fuse build com.opera.Opera.ffmpeg.yaml
+date
